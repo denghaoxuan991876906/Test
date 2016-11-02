fbb8b95a6fd324b50e4f572c1d4a9da0473cdb5c
Deng Lang <991876906@qq.com>
2016-11-02 12:20:35 +0800

    DENGLANG

---
+++ src/Demo1.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2016/11/2 0002.
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!!!");
+    }
+}
